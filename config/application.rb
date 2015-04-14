14e3c374f9f2674c830a367369550722d87cee2a
shunie99 <astroboy1985_28@hotmail.com>
2015-04-14 01:21:08 +0000

    Finish routes

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,8 @@ Bundler.require(*Rails.groups)
 
 module SampleHandover
   class Application < Rails::Application
+    
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
@@ -19,5 +21,6 @@ module SampleHandover
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
   end
 end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,1 @@
+SampleHandover::Application.config.secret_key_base = '<eb8cc2d2d020f58bddc3c431fde97272391c9ad1e6b63c1e00bcb03ab800e12b41cd584ed30ecd04691b3f85131f84f733536eeb127c8ff30650434a6a481661>'
